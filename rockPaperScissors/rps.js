97ae1f029415f188fec8ed6aa780aaa062fe0115
jsmithBadin <135791484+jsmithBadin@users.noreply.github.com>
2024-02-08 13:06:32 -0500

    Improved comment for getComputerChoice function.  Added Comment for console.log tester

--- rockPaperScissors/rps.js
+++ rockPaperScissors/rps.js
@@ -1,7 +1,7 @@
 console.log("Testing 1, 2, 3, can anybody hear me?");
 /*Rock, Paper, Scissors simulator is a program that allows the user to play Rock, Paper, Scissors, against a computer opponent.*/
 
-//Make a function called getComputerChoice that determines what the computer is going to "throw" at the player. 
+//Make a function called getComputerChoice that determines what the computer is going to "throw" at the player. We expect the computer to choose each of Rock, Paper, and Scissors 1/3 of the time.
 function getComputerChoice(){
     const roll = Math.random();
     if (roll < (1/3)){
@@ -15,4 +15,6 @@ function getComputerChoice(){
     }
 }
 
+// console.log to test that getComputerChoice is working.  Keeping this for now so that I can verify that the game plays by the rules when fully coded.
 console.log(getComputerChoice());
+
