bd7d27559727459f438d891576fd21d3a321af51
jsmithBadin <135791484+jsmithBadin@users.noreply.github.com>
2024-02-09 13:12:35 -0500

    removed testing code, and unnecessary comments about them

--- rockPaperScissors/rps.js
+++ rockPaperScissors/rps.js
@@ -15,17 +15,12 @@ function getComputerChoice(){
     }
 }
 
-// console.log to test that getComputerChoice is working.  
-//Keeping this for now so that I can verify that the game plays by the rules when fully coded.
-/*console.log(getComputerChoice());*/
-
 // make a function, play to play a round of RPS, using parameters "playerSelection" and "computerSelection".  
-//The function returns a string announcing result.
+// The function returns a string announcing result.
 //Convert inputs to upper case, so case doesn't cause comparison problems.
 //If inputs match, declare a tie
 //Else, rock beats scissors, scissors beats paper, paper beats rock
 //N.B. 4th condition exists in case the player does not send a proper input
-//Edit note: changing this function to return whether computer or player won the round, and console.logging the winner of the round
 function playRound(playerSelection, computerSelection){
     const player = playerSelection.toUpperCase();
     const computer = computerSelection.toUpperCase();
@@ -68,15 +63,8 @@ function playRound(playerSelection, computerSelection){
     }
 }
 
-//Testing that playRound functions as expected:
-// Edit: commenting out so testing does not interfere with primary operation
-/*
-const playerSelection = "Scissors";
-const computerSelection = getComputerChoice();
-console.log(playRound(playerSelection, computerSelection));
-*/
-
-//Make playGame function to run the actual game.  Play 5 rounds for one game, winner of each round displayed through console.log, overall winner displayed at the end.  
+//Make playGame function to run the actual game.  Play 5 rounds for one game, winner of each round displayed through console.log, 
+//overall winner displayed at the end.  
 function playGame(){
     //create variables to track scores
     let playerScore = 0;
